d9874424e331e8b2cf709f0c490438cde088351d
marvinortiz22 <marvincubias22@gmail.com>
2024-02-18 22:34:31 -0600

    esqueleto

--- cargarProyectos.js
+++ cargarProyectos.js
@@ -1,33 +1,45 @@
 let proyectos = [
     {
-        nombre: "proyecto 1",
-        srcfoto: "./static/proyectoimage.png",
+        nombre: "RaidSim",
+        srcfoto: "./images/proyectos/RaidSim.png",
         descripcion: "Simulador de distintos tipos de arreglos RAID que permite ver como se distribuyen los discos duros que los componen, así como los datos que en ellos se almacenan.",
-        tecnologias: ["html","boostrap","js"]
+        tecnologias: ["html","boostrap","js"],
+        url:"https://marvinortiz22.github.io/RAIDSim/"
     },
     {
-        nombre: "proyecto 2",
-        srcfoto: "./static/proyectoimage.png",
-        descripcion: "Simulador de distintos tipos de arreglos RAID que permite ver como se distribuyen los discos duros que los componen, así como los datos que en ellos se almacenan.",
-        tecnologias: ["html","css","js"]
+        nombre: "Sistema gestor de notas",
+        srcfoto: "./images/proyectos/sistema gestor de notas.webp",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
+        tecnologias: ["html","boostrap","django","mysql"],
+        url:""
     },
     {
-        nombre: "proyecto 3",
-        srcfoto: "./static/proyectoimage.png",
-        descripcion: "Simulador de distintos tipos de arreglos RAID que permite ver como se distribuyen los discos duros que los componen, así como los datos que en ellos se almacenan.",
-        tecnologias: ["html","css","js"]
+        nombre: "Sistema de estados financieros",
+        srcfoto: "./images/proyectos/sistema de estados financieros.jpg",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
+        tecnologias: ["html","boostrap","django","mysql"],
+        url:""
     },
     {
-        nombre: "proyecto 4",
-        srcfoto: "./static/proyectoimage.png",
-        descripcion: "Simulador de distintos tipos de arreglos RAID que permite ver como se distribuyen los discos duros que los componen, así como los datos que en ellos se almacenan.",
-        tecnologias: ["html","css","js"]
+        nombre: "Sistema de gestion de proyectos viales",
+        srcfoto: "./images/proyectos/sistema de gestion de proyectos viales.jpg",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
+        tecnologias: ["html","boostrap","django","mysql"],
+        url:""
     },
     {
-        nombre: "proyecto 5",
-        srcfoto: "./static/proyectoimage.png",
-        descripcion: "Simulador de distintos tipos de arreglos RAID que permite ver como se distribuyen los discos duros que los componen, así como los datos que en ellos se almacenan.",
-        tecnologias: ["html","css","js"]
+        nombre: "Tekedafash",
+        srcfoto: "./images/proyectos/tekedafash.jpg",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
+        tecnologias: ["html","boostrap","django","mysql"],
+        url:""
+    },
+    {
+        nombre: "Sistema de reporte de accidente",
+        srcfoto: "./images/proyectos/sistema de reportes de transito.jpg",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
+        tecnologias: ["html","boostrap","js","django","postgress"],
+        url:""
     },
 ]
 function init() {
@@ -40,15 +52,15 @@ function cargarProyectos(){
         /*i==2?proyectoContainer=document.getElementById("proyectosocultos"):false*/
         proyectoContainer.innerHTML+=`
         <div class="cardproyecto" id="cardproyecto">
-            <img src="./static/github-icon.png" class="githubproyectoicon">
+            <img src="./images/github-icon.png" class="githubproyectoicon">
             <p class="nombreproyecto">${proyectos[i].nombre}</p>
             <img class="proyectoimage" id="proyectoimage" src="${proyectos[i].srcfoto}">
             <p id="descripcion" class="proyecto-descripcion">${proyectos[i].descripcion}</p>
             <p class="tecnologias">Tecnologías: </p>
             <div class="tecnologiasiconos">
-            ${proyectos[i].tecnologias.map(tecnologia => `<img class="tecnologiaicono" src="./static/tecnologias/${tecnologia}.png"></img>`).join('')}
+            ${proyectos[i].tecnologias.map(tecnologia => `<img class="tecnologiaicono" title="${tecnologia}" src="./images/tecnologias/${tecnologia}.png"></img>`).join('')}
             </div>
-            <a href="" class="abotonvisitar"><button class="botonvisitar">Visitar</button></a>
+            <a href="${proyectos[i].url}" target="_blank" class="abotonvisitar"><button class="botonvisitar">Visitar</button></a>
         </div>`
 
     }
