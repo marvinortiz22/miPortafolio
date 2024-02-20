040cf46a7b711f0b9410ee97f2546a232567a668
marvinortiz22 <marvincubias22@gmail.com>
2024-02-19 22:44:50 -0600

    19-2

--- cargarProyectos.js
+++ cargarProyectos.js
@@ -3,43 +3,49 @@ let proyectos = [
         nombre: "RaidSim",
         srcfoto: "./images/proyectos/RaidSim.png",
         descripcion: "Simulador de distintos tipos de arreglos RAID que permite ver como se distribuyen los discos duros que los componen, así como los datos que en ellos se almacenan.",
-        tecnologias: ["html","boostrap","js"],
-        url:"https://marvinortiz22.github.io/RAIDSim/"
+        tecnologias: ["HTML","Boostrap","JS"],
+        url:"https://marvinortiz22.github.io/RAIDSim/",
+        repositorio:"https://github.com/marvinortiz22/RAIDSim"
     },
     {
         nombre: "Sistema gestor de notas",
         srcfoto: "./images/proyectos/sistema gestor de notas.webp",
         descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
-        tecnologias: ["html","boostrap","django","mysql"],
-        url:""
+        tecnologias: ["HTML","Boostrap","Django","MySQL"],
+        url:"",
+        repositorio:""
     },
     {
         nombre: "Sistema de estados financieros",
         srcfoto: "./images/proyectos/sistema de estados financieros.jpg",
         descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
-        tecnologias: ["html","boostrap","django","mysql"],
-        url:""
+        tecnologias: ["HTML","Boostrap","Django","MySQL"],
+        url:"",
+        repositorio:""
     },
     {
         nombre: "Sistema de gestion de proyectos viales",
         srcfoto: "./images/proyectos/sistema de gestion de proyectos viales.jpg",
         descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
-        tecnologias: ["html","boostrap","django","mysql"],
-        url:""
+        tecnologias: ["HTML","Boostrap","Django","MySQL"],
+        url:"",
+        repositorio:""
     },
     {
         nombre: "Tekedafash",
         srcfoto: "./images/proyectos/tekedafash.jpg",
         descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
-        tecnologias: ["html","boostrap","django","mysql"],
-        url:""
+        tecnologias: ["HTML","Boostrap","Django","MySQL"],
+        url:"",
+        repositorio:""
     },
     {
         nombre: "Sistema de reporte de accidente",
         srcfoto: "./images/proyectos/sistema de reportes de transito.jpg",
         descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa possimus nihil ipsum obcaecati quisquam rerum ducimus maxime optio corrupti at laboriosam architecto minus, sint consequuntur voluptates repellat atque doloribus nesciunt!",
-        tecnologias: ["html","boostrap","js","django","postgress"],
-        url:""
+        tecnologias: ["HTML","Boostrap","JS","Django","PostgreSQL"],
+        url:"",
+        repositorio:""
     },
 ]
 function init() {
@@ -52,7 +58,7 @@ function cargarProyectos(){
         /*i==2?proyectoContainer=document.getElementById("proyectosocultos"):false*/
         proyectoContainer.innerHTML+=`
         <div class="cardproyecto" id="cardproyecto">
-            <img src="./images/github-icon.png" class="githubproyectoicon">
+        <a href="${proyectos[i].repositorio}" class="agithubproyectoicon" target="_blank"><img src="./images/contacto/github-dark.png" title="Ir al repositorio" class="githubproyectoicon"></a>
             <p class="nombreproyecto">${proyectos[i].nombre}</p>
             <img class="proyectoimage" id="proyectoimage" src="${proyectos[i].srcfoto}">
             <p id="descripcion" class="proyecto-descripcion">${proyectos[i].descripcion}</p>
